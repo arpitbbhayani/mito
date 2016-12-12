1fbc83202480a6f223463dc3d60fe21331cdfc06
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2016-12-12 21:28:08 +0530

    Loading config from environment and defaults.

--- mito/__init__.py
+++ mito/__init__.py
@@ -1,7 +1,22 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+"""
+Load default settings from file `defaults.py`
+"""
+app.config.from_object('mito.defaults')
+
+"""
+Overriding default settings from Environment variables.
+"""
+overridden_config = {k:v for k, v in os.environ.items() if k in app.config}
+app.config.update(overridden_config)
+
+"""
+Registering all blueprints.
+"""
 from mito.views import pages, status
 
 app.register_blueprint(pages.mod)
---
+++ mito/defaults.py
@@ -0,0 +1,14 @@
+"""
+Setting for Flask-Login
+"""
+SECRET_KEY = 'default-secret-key'
+
+"""
+List containing MongoDB Database connection params.
+"""
+DB_JSON = '[]'
+
+"""
+Users that are allowed to login and access the system.
+"""
+ALLOWED_USERS = '[]'
