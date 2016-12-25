c315c899c6724013b880a26222936a8d501c6bad
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2016-12-25 16:52:02 +0530

    Fetch all info about unsubscribed companies during subscription page

--- mito/render_helpers/user_subscriptions_page.py
+++ mito/render_helpers/user_subscriptions_page.py
@@ -1,6 +1,7 @@
 from flask import render_template
 
 from mito import app
+from mito.services import user_subscriptions_service, company_service
 
 
 def render_user_subscriptions_action_buttons():
@@ -8,9 +9,29 @@ def render_user_subscriptions_action_buttons():
         return render_template('user_subscriptions_page/user_subscriptions_actions.html')
 
 
-def render_user_subscription(user_subscription):
+def render_user_subscription(user_id):
     with app.app_context():
+        user_subscription, error = user_subscriptions_service.get_subscriptions_for_user(user_id)
+        if error:
+            return error.description
         return render_template('user_subscriptions_page/user_subscriptions.html', user_subscription=user_subscription)
+
+
+def render_user_unsubscribed_companies(user_id):
+    with app.app_context():
+        user_subscription, error = user_subscriptions_service.get_subscriptions_for_user(user_id)
+        if error:
+            return error.description
+
+        companies, error = company_service.get_all_active_companies()
+        if error:
+            return error.description
+
+        unsubscribed_companies = [company for company in companies
+                                  if company.id not in
+                                  set(user_subscription.company_ids)]
+
+        return render_template('user_subscriptions_page/user_unsubscribed_companies.html', unsubscribed_companies=unsubscribed_companies)
 
 
 def render_layout():
