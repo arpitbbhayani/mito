b7300ab1887d2a650a0d0bf3b18bcd51e2ba8703
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2016-12-29 22:31:01 +0530

    Adding buttons and pages for article depending on state

--- mito/render_helpers/articles_page.py
+++ mito/render_helpers/articles_page.py
@@ -4,6 +4,16 @@ from mito import app
 from mito.services import user_bucket_service
 
 
+def render_articles_home_layout():
+    with app.app_context():
+        return render_template('articles_page/home_layout.html')
+
+
+def render_articles_home_action_buttons():
+    with app.app_context():
+        return render_template('articles_page/articles_home_actions.html')
+
+
 def render_layout():
     with app.app_context():
         return render_template('articles_page/layout.html')
@@ -16,6 +26,6 @@ def render_articles(user_id, article_state):
         return render_template('articles_page/articles.html', articles=articles)
 
 
-def render_articles_action_buttons(article_state):
+def render_articles_action_buttons():
     with app.app_context():
-        return render_template('articles_page/articles_actions.html', article_state=article_state)
+        return render_template('articles_page/articles_actions.html')
--- mito/views/articles.py
+++ mito/views/articles.py
@@ -11,9 +11,8 @@ mod = Blueprint('articles', __name__, )
 @login_required
 def index():
     return LayoutSR(
-        Component('articles-actions', articles_page.render_articles_action_buttons, 'pending'),
-        Component('articles', articles_page.render_articles, current_user.id, 'pending'),
-        layout=Layout(articles_page.render_layout),
+        Component('articles-home-actions', articles_page.render_articles_home_action_buttons),
+        layout=Layout(articles_page.render_articles_home_layout),
         pre_stream=(Dom(common_page.render_pre_body),
                     Dom(common_page.render_top_menu, current_user.is_authenticated)),
         post_stream=Dom(common_page.render_post_body)
@@ -24,7 +23,7 @@ def index():
 @login_required
 def get_articles_by_state(article_state):
     return LayoutSR(
-        Component('articles-actions', articles_page.render_articles_action_buttons, article_state),
+        Component('articles-actions', articles_page.render_articles_action_buttons),
         Component('articles', articles_page.render_articles, current_user.id, article_state),
         layout=Layout(articles_page.render_layout),
         pre_stream=(Dom(common_page.render_pre_body),
